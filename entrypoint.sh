20f0c16027a069e747c29b3164a3d322d41f7d2e
priyadharshinib <priyadharshinib@shorepointsys.com>
2024-12-17 11:09:32 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+set -e
+
+echo Waiting for database...
+
+while ! nc -z ${ODOO_DATABASE_HOST} ${ODOO_DATABASE_PORT} 2>&1; do sleep 1; done; 
+
+echo Database is now available
+
+exec odoo \
+    --http-port="${PORT}" \
+    --init=all \
+    --without-demo=True \
+    --proxy-mode \
+    --db_host="${ODOO_DATABASE_HOST}" \
+    --db_port="${ODOO_DATABASE_PORT}" \
+    --db_user="${ODOO_DATABASE_USER}" \
+    --db_password="${ODOO_DATABASE_PASSWORD}" \
+    --database="${ODOO_DATABASE_NAME}" \
+    --smtp="${ODOO_SMTP_HOST}" \
+    --smtp-port="${ODOO_SMTP_PORT_NUMBER}" \
+    --smtp-user="${ODOO_SMTP_USER}" \
+    --smtp-password="${ODOO_SMTP_PASSWORD}" \
+    --email-from="${ODOO_EMAIL_FROM}" 2>&1
